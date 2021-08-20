9aee2ae097885f8428ee9752de92dbdff377e359
arthur.klezovich <klezovich@gmail.com>
2021-08-20 15:54:50 +0200

    Add JSON endpoint

--- src/main/kotlin/com/klezovich/ExampleResource.kt
+++ src/main/kotlin/com/klezovich/ExampleResource.kt
@@ -1,7 +1,9 @@
 package com.klezovich
 
+import org.postgresql.core.Oid.JSON
 import java.util.*
 import javax.ws.rs.GET
+import javax.ws.rs.POST
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
@@ -17,4 +19,16 @@ class ExampleResource {
     @Path("/uuid")
     @Produces(MediaType.TEXT_PLAIN)
     fun uuid() = UUID.randomUUID()
+
+    @POST
+    fun post() = "Post received"
+
+    @GET
+    @Path("/car")
+    @Produces(MediaType.APPLICATION_JSON)
+    fun getCar() = Car()
+
+    class Car(val name:String="BMW") {
+
+    }
 }
