ee9e8765b7a54c89209d450b4abd08ff49192bea
arthur.klezovich <klezovich@gmail.com>
2021-08-23 17:57:53 +0200

    Add examples of using @ConfigurationProperty

--- src/main/kotlin/com/klezovich/resource/GreetingService.kt
+++ src/main/kotlin/com/klezovich/resource/GreetingService.kt
@@ -1,9 +1,20 @@
 package com.klezovich.resource
 
+import org.eclipse.microprofile.config.inject.ConfigProperty
+import java.util.*
 import javax.enterprise.context.ApplicationScoped
 
 @ApplicationScoped
 class GreetingService {
 
-    fun getGreeting() = "Hello from service"
+    @ConfigProperty(name="greeting.name")
+    lateinit var name:String
+
+    @ConfigProperty(name="greeting.suffix", defaultValue = "MAS!")
+    lateinit var suffix:String
+
+    @ConfigProperty(name="greeting.prefix")
+    lateinit var prefix: Optional<String>
+
+    fun getGreeting() = """${prefix.orElseGet { "??" }} Hello from $name $suffix"""
 }
