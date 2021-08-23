64089c7739fa29d4b897f95bd827c135f802276f
arthur.klezovich <klezovich@gmail.com>
2021-08-23 15:27:11 +0200

    Add code with mock injection

--- src/test/kotlin/com/klezovich/resource/GreetingResourceTest.kt
+++ src/test/kotlin/com/klezovich/resource/GreetingResourceTest.kt
@@ -1,21 +1,28 @@
 package com.klezovich.resource
 
 import io.quarkus.test.junit.QuarkusTest
+import io.quarkus.test.junit.mockito.InjectMock
 import io.restassured.RestAssured.given
 import org.hamcrest.Matchers.contains
 import org.hamcrest.Matchers.containsString
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito.`when`
 
 @QuarkusTest
 internal class GreetingResourceTest {
 
+    @InjectMock
+    lateinit var greetingService: GreetingService
+
     @Test
     fun testGreeting() {
+        `when`(greetingService.getGreeting())
+            .thenReturn("AK Hello!")
+
         given()
             .`when`().get("/greeting")
             .then()
             .statusCode(200)
-            .body(containsString("Hello"))
+            .body(containsString("AK Hello!"))
     }
 }
