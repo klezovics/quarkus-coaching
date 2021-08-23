46c410a9dbee00f422dc1cbabd6877c9ee1a2497
arthur.klezovich <klezovich@gmail.com>
2021-08-23 16:19:00 +0200

    Add readiness check

--- src/main/kotlin/com/klezovich/observability/ReadinessCheck.kt
+++ src/main/kotlin/com/klezovich/observability/ReadinessCheck.kt
@@ -3,6 +3,7 @@ package com.klezovich.observability;
 import io.quarkus.runtime.StartupEvent
 import org.eclipse.microprofile.health.HealthCheck
 import org.eclipse.microprofile.health.HealthCheckResponse
+import org.eclipse.microprofile.health.HealthCheckResponseBuilder
 import org.eclipse.microprofile.health.Readiness;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -19,7 +20,13 @@ class ReadinessCheck : HealthCheck {
     }
 
     override fun call(): HealthCheckResponse {
-        if(ready) return HealthCheckResponse.up("All good!")
+        if(ready) {
+            val b = HealthCheckResponse.builder()
+            b.up()
+            b.name("Main readiness check")
+            b.withData("Gotcha", "RAM looks kind of slow")
+            return b.build()
+        }
         return HealthCheckResponse.down("Not up!")
     }
 
